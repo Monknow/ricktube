3608dd0f6545937d6980c821b7eef06c07e04e93
Monknow <juanrodriguezcode@gmail.com>
2021-09-18 19:26:48 -0400

    Se ha añadido página de politica de usuario

---
+++ src/pages/policy.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const PolicyPage = () => {
+	return (
+		<div>
+			This site doesn't sell your data. I made it for my portfolio and I don't even know where I could sell it :D
+		</div>
+	);
+};
+
+export default PolicyPage;
